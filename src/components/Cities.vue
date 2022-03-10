858ea065f37003cfcf811ffcaf18d99ba79493f6
alpsenell <alp@useinsider.com>
2022-03-10 22:57:05 +0300

    lecture 2

---
+++ src/components/Cities.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <b-dropdown id="dropdown-1" text="Sehir Seciniz" class="m-md-2">
+            <b-dropdown-item
+                v-for="city in availableCities"
+                :key="city"
+                @click="selectCity(city)">
+                <span class="extra-text" v-if="city === userLocatedCity">Suan buradasiniz !</span>
+                <span class="extra-text" v-else-if="city === 'ankara'">Suan ankarradasin !</span>
+                <span class="extra-text" v-else>konumun bilinmiyor</span>
+                {{ city }}
+            </b-dropdown-item>
+        </b-dropdown>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Cities",
+
+    data() {
+        return {
+            availableCities: ['istanbul', 'izmir', 'ankara', 'sirnak', 'hatay', 'antalya'],
+            userLocatedCity: 'istanbul'
+        }
+    },
+
+    methods: {
+        selectCity(city) {
+            this.$emit('changeCity', city)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .extra-text {
+        font-size: 10px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from "axios";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import Foo from "./components/Foo";
+import MainPage from "./components/MainPage";
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+const routes = [
+  { path: '/', component: MainPage },
+];
+const availableCities = ['istanbul', 'izmir', 'ankara', 'sirnak', 'hatay', 'antalya'];
+
+availableCities.forEach(city => routes.push({ path: `/${city}`, component: Foo }));
+
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
+Vue.prototype.axios = axios;
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueRouter)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
